de1d803f137379b1314cbe5b2a7cc9b236695ce7
Eremeev Andery <a.yeremyeyev@gmail.com>
2020-03-03 10:55:00 +0200

    03_03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftComponent } from './left/left.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftComponent,
+    ContentComponent,
+    FooterComponent
+
   ],
   imports: [
     BrowserModule,
